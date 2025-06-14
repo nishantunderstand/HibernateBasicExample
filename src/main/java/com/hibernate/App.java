dc51075058bdf270493938dee323d413e46495b5
NishantRaj <nishant.read@gmail.com>
2025-06-14 14:01:57 +0530

    LenovoBat

--- src/main/java/com/hibernate/App.java
+++ src/main/java/com/hibernate/App.java
@@ -1,6 +1,8 @@
 package com.hibernate;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import com.hibernate.entity.Student;
 import com.hibernate.util.HibernateUtil;
@@ -11,7 +13,41 @@ import com.hibernate.util.HibernateUtil;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("===  Hibernate Working ====");
-		Student 
 		
 		
-		SessionFactory sessionFactory = HibernateUtil.getSessionFa
+		// 1 
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		//System.out.println(sessionFactory);
+		
+		// 2
+		Student student = new Student();
+		student.setName("Ankit");
+		student.setCollege("GGSIPU");
+		student.setActive(true);
+		student.setPhone("1234");
+		student.setFatherName("Dk");
+		
+		// 3
+		Session session = sessionFactory.openSession();
+		Transaction transaction = null;
+
+		//4
+		try {
+			transaction=session.beginTransaction();
+			session.persist(student); // Return Void
+			transaction.commit();
+			System.out.println("== Student Saved Successfully === ");
+		} catch (Exception e) {
+			if(transaction!=null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+			
+		}finally {
+			session.close();
+		}
+		
+		
+	}
+
+}
