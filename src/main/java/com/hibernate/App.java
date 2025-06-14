0cd4a43a87dcdcc4a5b82b104eb96fe0f90f2cc3
NishantRaj <nishant.read@gmail.com>
2025-06-14 13:10:00 +0530

    LenovoBat

--- src/main/java/com/hibernate/App.java
+++ src/main/java/com/hibernate/App.java
@@ -1,5 +1,10 @@
 package com.hibernate;
-
+/**
+ * Jun 14, 2025,12:52:39 PM
+ */
 public class App {
+	public static void main(String[] args) {
+		System.out.println("===  Hibernate Working ====");
+	}
 
 }
